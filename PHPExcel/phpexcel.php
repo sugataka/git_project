f0f61a12a6fb3374f5d68250f367e8b7ee375114
unknown <t.sugahara@hottea.jp>
2010-11-13 16:48:48 +0900

    test01

---
+++ PHPExcel/phpexcel.php
@@ -0,0 +1,14 @@
+<?php
+/* 
+ * PHPExcel
+ */
+ini_set('include_path', get_include_path() . PATH_SEPARATOR . VENDORS. "phpexcel" . DS . "Classes");
+
+
+/** Main Class */
+require_once 'Classes/PHPExcel.php';
+
+/** IO Class */
+require_once 'Classes/PHPExcel/IOFactory.php';
+
+?>
